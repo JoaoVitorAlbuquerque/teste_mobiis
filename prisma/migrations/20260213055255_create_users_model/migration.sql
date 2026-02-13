48cfa08e501e796dc7895fea0bb29f1124abb819
JoaoVitorAlbuquerque <joaovitorparussoloalbuquerque@gmail.com>
2026-02-13 02:56:03 -0300

    feat: create migration

---
+++ prisma/migrations/20260213055255_create_users_model/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "Nationality" AS ENUM ('BR', 'FOREIGN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "nationality" "Nationality" NOT NULL,
+    "document" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_document_key" ON "users"("document");
